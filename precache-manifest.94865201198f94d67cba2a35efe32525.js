1ba8ceb1cc5b76894e990d5858d325564b318836
thearjun <mailarjunadhikari@gmail.com>
2020-04-24 02:12:24 +0545

    Updates

---
+++ precache-manifest.94865201198f94d67cba2a35efe32525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f220ebb996e86b6df1d25d6da89194",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8e79cce610b51ff7b960",
+    "url": "/todo-app/static/css/main.dde6f756.chunk.css"
+  },
+  {
+    "revision": "056b783a6a21a74b42c9",
+    "url": "/todo-app/static/js/2.d17244af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.d17244af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e79cce610b51ff7b960",
+    "url": "/todo-app/static/js/main.f8b176b4.chunk.js"
+  },
+  {
+    "revision": "8c574afb4aca7e7fd22e",
+    "url": "/todo-app/static/js/runtime-main.e86c861d.js"
+  }
+]);
